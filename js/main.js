c52161141682167390fe29462bffd6398339888f
k-kyg <kygkd38@gmail.com>
2020-05-04 20:59:22 +0900

    やっぱTSにした

---
+++ js/main.js
@@ -0,0 +1,3 @@
+"use strict";
+var logo = document.querySelector(".logo");
+logo === null || logo === void 0 ? void 0 : logo.addEventListener("click", function () { return location.href = "index.html"; });
---
+++ ts/main.ts
@@ -0,0 +1,2 @@
+const logo: Element | null = document.querySelector(".logo");
+logo?.addEventListener("click", () => location.href = "index.html");
